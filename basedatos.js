a4e3874aae019e3ca59d55a70b2f9088c8b1f710
Juan C Rosero <57236205+JuanK120@users.noreply.github.com>
2020-09-19 23:41:20 -0500

    Add files via upload

--- basedatos.js
+++ basedatos.js
@@ -1,17 +1,18 @@
 const { Pool } = require('pg');
 const Router = require('express-promise-router');
-
+connect.bodyParser()
 const pool = new Pool({
-  user: 'fredyb',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'univalle',
+  user: 'zrsgimxm',
+  host: 'lallah.db.elephantsql.com',
+  database: 'zrsgimxm',
+  password: 'aP5NiukxgElWrcMHCJ_hlNfcvRud3T85',
   port: 5432,
 });
 
 const router = new Router();
 // export our router to be mounted by the parent application
 module.exports = router;
+
 
 router.get('/consultatotalpacientes', async (req, res) => {
   //const { id } = req.params
@@ -25,4 +26,22 @@ router.post('/insertarpacientes', async (req, res) => {
     `INSERT INTO pacientes(nombre, apellido, numid) VALUES('${nombre}','${apellido}','${numid}')`
   );
   res.send('INSERTADO');
+});
+
+router.put('/acutalizarpacientes', async (req, res) => {
+  const { nombre, apellido, numid } = req.body;
+  await pool.query(
+    `UPDATE pacientes
+      SET nombre = '${nombre}', apellido = '${apellido}'
+    WHERE numid = '${numid}'; `
+  );
+  res.send('actualizado');
+});
+
+router.delete('/borrarpacientes', async (req, res) => {
+  const { nombre, apellido, numid } = req.body;
+  await pool.query(
+    `DELETE FROM pacientes WHERE numid = '${numid}' ;`
+  );
+  res.send('borrado');
 });
